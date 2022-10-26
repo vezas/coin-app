368094283decf13830063e3e6809ef8a988e1f7d
Radek <radoslaw.wrona1@gmail.com>
2022-10-26 14:09:02 +0200

    Update logic

--- src/components/coin/Coin.tsx
+++ src/components/coin/Coin.tsx
@@ -14,10 +14,11 @@ interface ApiResponse {
 
 export const Coin: React.FC<InterfaceCoin> = ({ id }) => {
   const [coinInfo, setcoinInfo] = useState({ url: '', name: '', price: '', ath: '' })
+  const [isLoading, setIsLoading] = useState(false)
 
   const fetchCoinInfoHandler = useCallback(async () => {
+    setIsLoading(true)
     const data: ApiResponse = await coinGeckoApi.get(`coins/${id}`)
-
     const {
       image: { large: url },
       name,
@@ -29,6 +30,7 @@ export const Coin: React.FC<InterfaceCoin> = ({ id }) => {
       }
     } = data
     setcoinInfo({ url, name, price, ath })
+    setIsLoading(false)
   }, [id])
 
   useEffect(() => {
@@ -37,11 +39,16 @@ export const Coin: React.FC<InterfaceCoin> = ({ id }) => {
 
   return (
     <React.Fragment>
-      <img className={classes.coinItem__logo} src={coinInfo.url} alt='Logo with coin.' />
-      <h1 className={classes.coinItem__header}>{coinInfo.name}</h1>
-      <h3 className={classes.coinItem__price}>Current price: {coinInfo.price} $</h3>
-      <h3 className={classes.coinItem__price}>All time high: {coinInfo.ath} $</h3>
-      <p>And so on...</p>
+      {isLoading && <p className={classes.coinItem_isLoading}>Loading...</p>}
+      {!isLoading && (
+        <div>
+          <img className={classes.coinItem__logo} src={coinInfo.url} alt='Logo with coin.' />
+          <h1 className={classes.coinItem__header}>{coinInfo.name}</h1>
+          <h3 className={classes.coinItem__price}>Current price: {coinInfo.price} $</h3>
+          <h3 className={classes.coinItem__price}>All time high: {coinInfo.ath} $</h3>
+          <p>And so on...</p>
+        </div>
+      )}
     </React.Fragment>
   )
 }
